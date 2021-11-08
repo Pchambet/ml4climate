85dec52371e9aad5815fb8ca066249f65b86399b
Tantet Alexis (M.) <alexis.tantet@lmd.polytechnique.fr>
2021-11-08 03:42:44 +0100

    increase pandas version

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 name = 'machine_learning_for_climate_and_energy'
 reqs = ['bokeh >=2.2.3', 'hvplot', 'matplotlib', 'netcdf4', 'numpy',
-        'pandas', 'panel >=0.10.3', 'scipy', 'scikit-learn >=1.0', 'xarray']
+        'pandas >=1.05', 'panel >=0.10.3', 'scipy', 'scikit-learn >=1.0',
+        'xarray']
 
 setup(name=name, install_requires=reqs)
