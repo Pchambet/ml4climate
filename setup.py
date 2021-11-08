551e2a5e97c97131dc0e4d9e11dff0f671ebed51
Tantet Alexis (M.) <alexis.tantet@lmd.polytechnique.fr>
2021-11-08 03:43:50 +0100

    debug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 name = 'machine_learning_for_climate_and_energy'
 reqs = ['bokeh >=2.2.3', 'hvplot', 'matplotlib', 'netcdf4', 'numpy',
-        'pandas >=1.05', 'panel >=0.10.3', 'scipy', 'scikit-learn >=1.0',
+        'pandas >=1.0.5', 'panel >=0.10.3', 'scipy', 'scikit-learn >=1.0',
         'xarray']
 
 setup(name=name, install_requires=reqs)
